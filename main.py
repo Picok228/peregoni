347f5e2f91d265b9baff1d010724bfbc84df7042
Picok228 <152019836+Picok228@users.noreply.github.com>
2024-05-25 20:34:20 +0300

    peregoni

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def start_game():
     fps = pygame.time.Clock()
 
 
-    roket = Player(270, 290, 65, 145, "optimys_prime/автомобіль.png", 7)
-
+    roket = Player(270, 290, 65, 135, "optimys_prime/автомобіль.png", 3)
+    bacground = pygame.transform.scale(
+        pygame.image.load("optimys_prime/дорога.png"), (500, 650)
+    )
 
     while True:
         for event in pygame.event.get():
@@ -19,5 +21,6 @@ def start_game():
         roket.move()
         pygame.display.flip()
         fps.tick(30)
+        window.blit(bacground, (5, 5))
 
 start_game()
